fc53c00a0faab0ff14bc5bb12bc7994019790c4f
Leonid Ragunovich <leonid.makintosh@yandex.by>
2019-09-07 19:19:25 +0300

    fix encryptionNotice showing if encryption disabled

--- src/gui/wizard/owncloudadvancedsetuppage.cpp
+++ src/gui/wizard/owncloudadvancedsetuppage.cpp
@@ -124,6 +124,7 @@ void OwncloudAdvancedSetupPage::initializePage()
 #ifdef ADD_ENCRYPTION
     updateEncryptionUi(goodLocalFolder);
 #else
+    _ui.encryptionNotice->setVisible(false);
     _ui.errorLabel->setVisible(false);
     _ui.passwords_label->setVisible(false);
     _ui.passwords->setVisible(false);
